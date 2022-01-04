74c465910245867c8bb137f065c204b9e0e388d4
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-01-04 13:48:14 +0700

    add

---
+++ js.js
@@ -0,0 +1,31 @@
+// let i = 10;
+// let f = 20.5;
+// let b = true;
+// let s = 'Ha Noi';
+//
+// document.write('i =' + i)
+// document.write('<br/>')
+// document.write('f = ' + f)
+// document.write('<br/>')
+// document.write('b =' + b)
+// document.write('<br/>')
+// document.write('s =' + s)
+// document.write('<br/>')
+
+//Bai 2//
+// let width = 20;
+// let height = 10;
+// let area = width * height;
+// document.write('Area =' + area )
+
+//Bai 3
+
+a = prompt('Nhap bien a');
+b = prompt('Nhap bien b');
+if (a % b == 0) {
+    alert('a chia het cho b');
+}else {
+    alert('a khong chia het cho b');
+}
+
+
